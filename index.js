1b26c5f275713a93c64d6fce97346bf2f7752816
ZhouJas <jasonzhou460@gmail.com>
2021-03-03 16:53:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const client = new Discord.Client();
 
 const prefix = "%";
 
-client.on("message", function(message) { 
+client.on("message", async function (message) {
     if (message.author.bot) {
         return;
     }
-    if(message.content == "YEP") {
+    if (message.content == "YEP") {
         message.channel.send("COCK!");
     }
     if (!(message.content.startsWith(prefix))) {
@@ -24,8 +24,27 @@ client.on("message", function(message) {
         const timeTaken = Date.now() - message.createdTimestamp;
         message.channel.send(`:ping_pong: ${timeTaken}ms.`);
     }
+    if (command === "pin") {
+        var chnl = message.channel;
+        if (message.reference) {
+            var referencedMessageID = message.reference.messageID;
+            console.log(referencedMessageID);
+            var referencedMessage;
+             await chnl.messages.fetch(referencedMessageID).then(foundMessage => referencedMessage=foundMessage).catch(console.error);
+             await referencedMessage.pin({ reason: 'goose bot pin command invoked' }).then(result => {
+                Log.success(`Pin request success for Message: ${referencedMessage.content}`);
+            }).catch(console.error)
+            return;
+        } else {
+            chnl.send("No referenced message for pinning! Use the reply feature to add a referenced message and try again.").then(sentMessage => {
+                setTimeout(function () { sentMessage.delete(); }, 5000)
+            })
+        }
 
-});    
+
+    }
+
+});
 
 
 client.login(config.BOT_TOKEN);
