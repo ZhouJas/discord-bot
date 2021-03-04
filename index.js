e09916b761178f6f98ece540c3201c4a15f13e42
ZhouJas <jasonzhou460@gmail.com>
2021-03-03 20:02:30 -0500

    Remove console log

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ client.on("message", async function (message) {
         var chnl = message.channel;
         if (message.reference) {
             var referencedMessageID = message.reference.messageID;
-            console.log(referencedMessageID);
             var referencedMessage;
             await chnl.messages.fetch(referencedMessageID).then(foundMessage => referencedMessage = foundMessage).catch(console.error);
             await referencedMessage.pin({ reason: 'goose bot pin command invoked' }).then(result => {
